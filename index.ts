06cde638ac64fae05b5013030c8cb678b3e4919a
Dustin Ecker <dustin.ecker12@gmail.com>
2023-04-14 10:15:23 -0400

    basic http server for a wordle api

---
+++ index.ts
@@ -0,0 +1,24 @@
+import express, { Application, Request, Response } from 'express';
+import wordList from './wordList';
+
+const app: Application = express();
+const PORT = process.env.PORT ?? 8000;
+
+type Word = {
+  secret: string;
+};
+
+app.get('/secret', (req: Request, res: Response) => {
+  const randomIndex = Math.floor(Math.random() * wordList.length) + 1;
+  const word = wordList[randomIndex];
+
+  const secret: Word = {
+    secret: word,
+  };
+
+  res.json(secret);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
